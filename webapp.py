91f3ea37a3f2ea2b89a29982575a02eaec95d5cb
cnarte <ankitkrchy.54@gmail.com>
2024-03-02 14:05:15 +0530

    formatted

--- webapp.py
+++ webapp.py
@@ -12,18 +12,18 @@ from markdown import markdown
 from utils import haystack_is_ready, query, send_feedback, upload_doc, haystack_version, get_backlink
 
 
-# Adjust to a question that you would like users to see in the search bar when they load the UI:
+
 DEFAULT_QUESTION_AT_STARTUP = os.getenv("DEFAULT_QUESTION_AT_STARTUP", "")
 DEFAULT_ANSWER_AT_STARTUP = os.getenv("DEFAULT_ANSWER_AT_STARTUP", "")
 
-# Sliders
+
 DEFAULT_DOCS_FROM_RETRIEVER = int(os.getenv("DEFAULT_DOCS_FROM_RETRIEVER", "3"))
 DEFAULT_NUMBER_OF_ANSWERS = int(os.getenv("DEFAULT_NUMBER_OF_ANSWERS", "3"))
 
-# Labels for the evaluation
+
 EVAL_LABELS = os.getenv("EVAL_FILE", str(Path(__file__).parent / "eval_labels_example.csv"))
 
-# Whether the file upload should be enabled or not
+
 DISABLE_FILE_UPLOAD = bool(os.getenv("DISABLE_FILE_UPLOAD"))
 
 
@@ -36,21 +36,21 @@ def main():
 
     st.set_page_config(page_title="Legal Document Retriver Demo", page_icon="https://haystack.deepset.ai/img/HaystackIcon.png")
 
-    # Persistent state
+    
     set_state_if_absent("question", DEFAULT_QUESTION_AT_STARTUP)
     set_state_if_absent("answer", DEFAULT_ANSWER_AT_STARTUP)
     set_state_if_absent("results", None)
     set_state_if_absent("raw_json", None)
     set_state_if_absent("random_question_requested", False)
 
-    # Small callback to reset the interface in case the text of the question changes
+    
     def reset_results(*args):
         st.session_state.answer = None
         st.session_state.results = None
         st.session_state.raw_json = None
 
-    # Title
-    st.write("# Legal Document Retriver Demo")
+    
+    st.write("
     st.markdown(
         """
 This demo is supposed to show how we can leverage Document Retrival for Lawyers during work. 
@@ -58,7 +58,7 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
         unsafe_allow_html=True,
     )
 
-    # Sidebar
+    
     st.sidebar.header("Options")
     top_k_reader = st.sidebar.slider(
         "Max. number of answers",
@@ -76,17 +76,17 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
         step=1,
         on_change=reset_results,
     )
-    # eval_mode = st.sidebar.checkbox("Evaluation mode")
+    
     debug = st.sidebar.checkbox("Show debug info")
 
-    # File upload block
+    
     if not DISABLE_FILE_UPLOAD:
-        st.sidebar.write("## File Upload:")
+        st.sidebar.write("
         data_files = st.sidebar.file_uploader(
             "upload", type=["pdf", "txt", "docx"], accept_multiple_files=True, label_visibility="hidden"
         )
         for data_file in data_files:
-            # Upload file
+            
             if data_file:
                 try:
                     raw_json = upload_doc(data_file)
@@ -130,18 +130,6 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
         unsafe_allow_html=True,
     )
 
-    # Load csv into pandas dataframe
-    # try:
-    #     df = pd.read_csv(EVAL_LABELS, sep=";")
-    # except Exception:
-    #     st.error(
-    #         f"The eval file was not found. Please check the demo's [README](https://github.com/deepset-ai/haystack/tree/main/ui/README.md) for more information."
-    #     )
-    #     sys.exit(
-    #         f"The eval file was not found under `{EVAL_LABELS}`. Please check the README (https://github.com/deepset-ai/haystack/tree/main/ui/README.md) for more information."
-    #     )
-
-    # Search bar
     question = st.text_input(
         value=st.session_state.question,
         max_chars=100,
@@ -151,45 +139,24 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
     )
     col1, _ = st.columns(2)
     col1.markdown("<style>.stButton button {width:100%;}</style>", unsafe_allow_html=True)
-    # col2.markdown("<style>.stButton button {width:100%;}</style>", unsafe_allow_html=True)
+    
 
-    # Run button
     run_pressed = col1.button("Run")
 
-    # Get next random question from the CSV
-    # if col2.button("Random question"):
-    #     reset_results()
-    #     new_row = df.sample(1)
-    #     while (
-    #         new_row["Question Text"].values[0] == st.session_state.question
-    #     ):  # Avoid picking the same question twice (the change is not visible on the UI)
-    #         new_row = df.sample(1)
-    #     st.session_state.question = new_row["Question Text"].values[0]
-    #     st.session_state.answer = new_row["Answer"].values[0]
-    #     st.session_state.random_question_requested = True
-    #     # Re-runs the script setting the random question as the textbox value
-    #     # Unfortunately necessary as the Random Question button is _below_ the textbox
-    #     if hasattr(st, "scriptrunner"):
-    #         raise st.scriptrunner.script_runner.RerunException(
-    #             st.scriptrunner.script_requests.RerunData(widget_states=None)
-    #         )
-    #     raise st.runtime.scriptrunner.script_runner.RerunException(
-    #         st.runtime.scriptrunner.script_requests.RerunData(widget_states=None)
-    #     )
     st.session_state.random_question_requested = False
 
     run_query = (
         run_pressed or question != st.session_state.question
     ) and not st.session_state.random_question_requested
 
-    # Check the connection
+    
     with st.spinner("⌛️ &nbsp;&nbsp; Retriver Backend is starting..."):
         if not haystack_is_ready():
             st.error("🚫 &nbsp;&nbsp; Connection Error. Is Haystack running?")
             run_query = False
             reset_results()
 
-    # Get results for query
+    
     if run_query and question:
         reset_results()
         st.session_state.question = question
@@ -213,22 +180,16 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
                 return
 
     if st.session_state.results:
-
-        # Show the gold answer if we use a question of the given set
-        # if eval_mode and st.session_state.answer:
-        #     st.write("## Correct answer:")
-        #     st.write(st.session_state.answer)
-
-        st.write("## Results:")
+        st.write("
 
         for count, result in enumerate(st.session_state.results):
             if result["answer"]:
                 answer, context = result["answer"], result["context"]
                 start_idx = context.find(answer)
                 end_idx = start_idx + len(answer)
-                # Hack due to this bug: https://github.com/streamlit/streamlit/issues/3190
+                
                 st.write(
-                    markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "#8ef")) + context[end_idx:]),
+                    markdown(context[:start_idx] + str(annotation(answer, "ANSWER", "
                     unsafe_allow_html=True,
                 )
                 source = ""
@@ -246,7 +207,7 @@ This demo is supposed to show how we can leverage Document Retrival for Lawyers 
                 st.write("**Relevance:** ", result["relevance"])
 
             if eval_mode and result["answer"]:
-                # Define columns for buttons
+                
                 is_correct_answer = None
                 is_correct_document = None
 
